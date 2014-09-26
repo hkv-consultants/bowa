36bd0675d85e4a7a1b6c27a7b680c412319f70cb
Remco Gerlich <remco@gerlich.nl>
2014-09-26 14:25:05 +0200

    New function for the inundation PNG

--- bowa/tests.py
+++ bowa/tests.py
@@ -6,7 +6,55 @@ from __future__ import print_function
 from django.test import TestCase
 
 
-class ExampleTest(TestCase):
+import numpy as np
 
-    def test_something(self):
-        self.assertEquals(1, 1)
+from bowa import models
+
+
+class TestColorToRgb(TestCase):
+    def test_white(self):
+        self.assertTrue(
+            (models.color_to_rgb("FFFFFF") ==
+             np.array([255, 255, 255, 255])).all())
+
+
+class TestInundationPng(TestCase):
+    def test_first_value(self):
+        # '5' should get the first color, D7191C = (215, 25, 28)
+        rgba = models.colored_image_from_inundation_data(
+            np.array([[5, 5], [5, 5]]), 0)
+
+        self.assertTrue(
+            (rgba == np.array([[[215, 25, 28, 255],
+                                [215, 25, 28, 255]],
+                               [[215, 25, 28, 255],
+                                [215, 25, 28, 255]]])).all())
+
+    def test_second_value(self):
+        # '15' should get the second color
+        rgba = models.colored_image_from_inundation_data(
+            np.array([[15, 15], [15, 15]]), 0)
+
+        self.assertTrue(
+            (rgba == np.array([[[230, 84, 55, 255],
+                                [230, 84, 55, 255]],
+                               [[230, 84, 55, 255],
+                                [230, 84, 55, 255]]])).all())
+
+    def test_last_value(self):
+        # '150' should get the last color
+        rgba = models.colored_image_from_inundation_data(
+            np.array([[150, 150], [150, 150]]), 0)
+
+        self.assertTrue(
+            (rgba == np.array([[[43, 131, 186, 255],
+                                [43, 131, 186, 255]],
+                               [[43, 131, 186, 255],
+                                [43, 131, 186, 255]]])).all())
+
+    def test_nodata(self):
+        # If nodata, then last value should be 0
+        rgba = models.colored_image_from_inundation_data(
+            np.array([[150, 150], [150, 150]]), 150)
+
+        self.assertTrue((rgba[:, :, 3] == 0).all())
